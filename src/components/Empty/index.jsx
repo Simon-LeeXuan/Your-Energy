210d9fa29dbdf0f94b848be3657cfb1cccdbff9c
xuan1616 <1141226917@qq.com>
2021-12-20 21:14:07 +0800

    comment

--- src/components/Empty/index.jsx
+++ src/components/Empty/index.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import s from './style.module.less'
 
+// If there is no data, show "No data temporarily"
 const Empty = ({ desc }) => {
   return <div className={s.empty}>
-    <img src="//s.yezgea02.com/1619144597039/empty.png" alt="暂无数据"/>
-    <div>{desc || '暂无数据'}</div>
+    <img src="//s.yezgea02.com/1619144597039/empty.png" alt="No data"/>
+    <div>{desc || 'No data'}</div>
   </div>;
 };
 
